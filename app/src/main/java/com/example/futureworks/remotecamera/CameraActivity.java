8c43d91c253728a5060f25b4f622781adbffcf39
MichaelReyes <michaelangelo.reyes19@gmail.com>
2016-09-22 13:18:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/futureworks/remotecamera/CameraActivity.java
@@ -0,0 +1,18 @@
+package com.example.futureworks.remotecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
